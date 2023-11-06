df9f532b6ec3a0b6d1df3cfe64542c5a654748c3
millionairesmile <lswoo1221@gmail.com>
2023-11-06 10:07:16 +0900

    complete

--- main.js
+++ main.js
@@ -22,8 +22,73 @@ function getTime() {
   let minutes = String(date.getMinutes()).padStart(2, "0");
   let seconds = String(date.getSeconds()).padStart(2, "0");
 
-  clock.innerText = `${hours}:${minutes}:${seconds} ${amPm}`;
+  clock.innerText = `${hours}:${minutes}:${seconds}  ${amPm}`;
 }
 
 getTime();
 setInterval(getTime, 1000);
+
+const QUOTE_LIST = "quoteList";
+
+function getQuotes() {
+  const quotes = document.querySelector(".quotes");
+  let savedQuotes = localStorage.getItem(QUOTE_LIST);
+
+  if (!savedQuotes) {
+    localStorage.setItem(QUOTE_LIST, JSON.stringify(["명언"]));
+
+    savedQuotes = localStorage.getItem(QUOTE_LIST);
+  }
+
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  quotes.innerText =
+    parsedQuotes[Math.floor(Math.random() * parsedQuotes.length)];
+}
+
+getQuotes();
+
+function onClickQuotes() {
+  const quotes = document.querySelector(".quotes");
+  const newQuote = document.querySelector(".new-quote");
+
+  quotes.style.display = "none";
+  newQuote.style.display = "block";
+}
+
+function onClickNewQuote() {
+  const quotes = document.querySelector(".quotes");
+  const newQuote = document.querySelector(".new-quote");
+  const newQuoteInput = document.querySelector(".new-quote-input");
+
+  if (!newQuoteInput.value) return;
+
+  let savedQuotes = localStorage.getItem(QUOTE_LIST);
+  let parsedQuotes = JSON.parse(savedQuotes);
+
+  parsedQuotes.push(newQuoteInput.value);
+
+  localStorage.setItem(QUOTE_LIST, JSON.stringify(parsedQuotes));
+
+  quotes.innerText = newQuoteInput.value;
+  newQuoteInput.value = "";
+
+  quotes.style.display = "block";
+  newQuote.style.display = "none";
+}
+
+async function getNft() {
+  const nftImg = document.querySelector(".nft-img");
+  const nftName = document.querySelector(".nft-name");
+  const nftDesc = document.querySelector(".nft-desc");
+
+  const response = await axios.get(
+    "https://olbm.mypinata.cloud/ipfs/QmRorh39WMRCWapGfZbDiCWyFmTDhB4XxcQg2wgAb8VTiy"
+  );
+
+  nftImg.src = response.data.image;
+  nftName.innerText = response.data.name;
+  nftDesc.innerText = response.data.description;
+}
+
+getNft();
